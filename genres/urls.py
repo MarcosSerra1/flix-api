8ad8eda67639f5dfd6b1b7cad910fe2ba7b3834e
Marcos Serra <marcosserra133@gmail.com>
2024-06-21 21:44:26 -0300

    🆕 feat: Adicionada nova rota para `genre_detail_view` em urls.py
    Incluída a rota `genres/<int:pk>/` para a view `genre_detail_view`, permitindo acesso aos detalhes de um gênero específico.

--- genres/urls.py
+++ genres/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from genres.views import genre_create_list_view
+from genres.views import genre_create_list_view, genre_detail_view
 
 
 urlpatterns = [
-    path('genres/', genre_create_list_view, name='genre')
+    path('genres/', genre_create_list_view, name='genre'),
+    path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view'),
 ]

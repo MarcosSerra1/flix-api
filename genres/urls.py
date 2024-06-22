04b964d70b0684d72e396deaa666804a02f73ca3
Marcos Serra <marcosserra133@gmail.com>
2024-06-22 15:27:25 -0300

    🔄 refactor: Atualizada URL para utilizar views baseadas em classes
    - Atualizada a definição da URL para `GenreRetrieveUpdateDestroyView`.

--- genres/urls.py
+++ genres/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from genres.views import GenreCreateListView, genre_detail_view
+from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 
 
 urlpatterns = [
     path('genres/', GenreCreateListView.as_view(), name='genre'),
-    path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view'),
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
 ]

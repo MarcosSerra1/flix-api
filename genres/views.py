1c87c0c8024badfe7b61335b934634a4e05a9d48
Marcos Serra <marcosserra133@gmail.com>
2024-06-22 15:26:14 -0300

    🔄 refactor: Atualizada view genre_detail_view para usar GenreRetrieveUpdateDestroyView
    🔄 refactor: Atualizada view genre_detail_view para usar GenreRetrieveUpdateDestroyView
    
    Refatorada a view `genre_detail_view` para utilizar a view baseada em classe `GenreRetrieveUpdateDestroyView`, seguindo o padrão do Django REST Framework.
    
    - Substituída a função view `genre_detail_view` por `GenreRetrieveUpdateDestroyView`.
    - `GenreRetrieveUpdateDestroyView` agora é usada como view baseada em classe para recuperação, atualização e destruição de instâncias do modelo `Genre`.
    - Adicionado método `delete` personalizado para retornar uma mensagem de sucesso ou erro ao deletar um gênero.

--- genres/views.py
+++ genres/views.py
@@ -1,10 +1,7 @@
-import json
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
-from genres.models import Genre
 from rest_framework import generics
+from genres.models import Genre
 from genres.serializers import GenreSerializer
+from django.http import JsonResponse
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
@@ -12,26 +9,21 @@ class GenreCreateListView(generics.ListCreateAPIView):
     serializer_class = GenreSerializer
 
 
-@csrf_exempt
-def genre_detail_view(request, pk):
-    genre = get_object_or_404(Genre, pk=pk)
+class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
 
-    if request.method == 'GET':
-        data = {'id': genre.id, 'name': genre.name}
-        return JsonResponse(data)
-    
-    elif request.method == 'PUT':
-        data = json.loads(request.body.decode('utf-8'))
-        genre.name = data['name']
-        genre.save()
-        return JsonResponse(
-            {'id': genre.id, 'name': genre.name},
-            status=200
-        )
-    
-    elif request.method == 'DELETE':
-        genre.delete()
-        return JsonResponse(
-            {'message': 'Gênero excluído com sucesso!'},
-            status=204,
-        )
+    def delete(self, *args, **kwargs):
+        instance = self.get_object()
+        try: 
+            instance.delete()
+            return JsonResponse(
+                {'message': 'Gênero deletado com sucesso.'}, 
+                status=204
+            )
+        
+        except Exception as e: 
+            return JsonResponse(
+                {'error': str(e)},
+                status=500
+            )

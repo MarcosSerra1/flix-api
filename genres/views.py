30923220278a3f9ea86aa770cde073f16fb20c25
Marcos Serra <marcosserra133@gmail.com>
2024-06-21 21:42:47 -0300

    🆕 feat: Criada a view `genre_detail_view` no app 'genres'
    Adicionada a view `genre_detail_view` para retornar detalhes de um gênero específico com base no ID (pk).

--- genres/views.py
+++ genres/views.py
@@ -1,6 +1,7 @@
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 from genres.models import Genre
 
 
@@ -15,4 +16,14 @@ def genre_create_list_view(request):
         data = json.loads(request.body.decode('utf-8'))
         new_genre = Genre(name=data['name'])
         new_genre.save()
-        return JsonResponse({'id': new_genre.id, 'name': new_genre.name}, status=201,)
+        return JsonResponse(
+            {'id': new_genre.id, 'name': new_genre.name},
+            status=201,
+        )
+
+
+@csrf_exempt
+def genre_detail_view(request, pk):
+    genre = get_object_or_404(Genre, pk=pk)
+    data = {'id': genre.id, 'name': genre.name}
+    return JsonResponse(data)

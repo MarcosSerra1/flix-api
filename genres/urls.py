54a7cd94d7d110c337aea4b7e9e4514b2ca7b071
Marcos Serra <marcosserra133@gmail.com>
2024-06-22 01:21:19 -0300

    🔄 modify: Atualizada URL para usar GenreCreateListView como view baseada em classe
    Atualizada a URL de `/genres/` para usar `GenreCreateListView.as_view()` em vez da função view `genre_create_list_view`, seguindo o padrão de views baseadas em classe do Django REST Framework.
    
    - Substituída a função view `genre_create_list_view` por `GenreCreateListView.as_view()` na definição de URL.
    - `GenreCreateListView` agora é usado como uma view baseada em classe para manipulação de listagem e criação de instâncias do modelo `Genre`.

--- genres/urls.py
+++ genres/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from genres.views import genre_create_list_view, genre_detail_view
+from genres.views import GenreCreateListView, genre_detail_view
 
 
 urlpatterns = [
-    path('genres/', genre_create_list_view, name='genre'),
+    path('genres/', GenreCreateListView.as_view(), name='genre'),
     path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view'),
 ]

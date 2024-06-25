3237aaf0ab457ed99b3c9e330e27f5bdd387ce29
Marcos Serra <marcosserra133@gmail.com>
2024-06-25 15:10:16 -0300

    � create: Criada view ActorCreateListView e ActorRetrieveUpdateDestroyView

--- actors/views.py
+++ actors/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics
+from actors.models import Actors
+from actors.serializers import ActorSerializer
+from django.http import JsonResponse
 
-# Create your views here.
+
+class ActorCreateListView(generics.ListCreateAPIView):
+    queryset = Actors.objects.all()
+    serializer_class = ActorSerializer
+
+
+class ActorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Actors.objects.all()
+    serializer_class = ActorSerializer
+
+    def delete(self, *args, **kwargs):
+        instance = self.get_object()
+        try:
+            instance.delete()
+            return JsonResponse(
+                {'message': 'Ator deletado com sucesso.'},
+                status=204
+            )
+        
+        except Exception as e:
+            return JsonResponse(
+                {'error': str(e)},
+                status=500
+            )

1a7c8129bf9e15ef163fcb85fc7b8d4369695efc
Marcos Serra <marcosserra133@gmail.com>
2024-06-25 19:14:27 -0300

    ✨ feat: Criado o modelo Movie no app 'movies'
    Adicionado o modelo Movie no app 'movies' com campos para título, gênero, data de lançamento, atores e resumo, além das relações apropriadas com os modelos Genre e Actor.

--- movies/models.py
+++ movies/models.py
@@ -11,7 +11,7 @@ class Movie(models.Model):
         related_name='movies'
     )
     release_date = models.DateField(null=True, blank=True)
-    actors = models.ManyToManyField(Actor,related_name='movies')
+    actors = models.ManyToManyField(Actor, related_name='movies')
     resume = models.TextField(null=True, blank=True)
 
     def __str__(self) -> str:

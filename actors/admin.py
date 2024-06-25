d08dd1220943973d2a479c9a51f68bba2c16f345
Marcos Serra <marcosserra133@gmail.com>
2024-06-25 14:41:12 -0300

    🛠️ config: Configurado admin para o modelo Actors com list_display no arquivo admin.py

--- actors/admin.py
+++ actors/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from actors.models import Actors
 
-# Register your models here.
+
+@admin.register(Actors)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'birthday', 'nationality')

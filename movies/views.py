b7797a02581c0ff3893b149579ac95daec2a32c3
Marcos Serra <marcosserra133@gmail.com>
2024-06-26 14:43:22 -0300

    ✨ feat: Criadas as views MovieCreateListView e MovieRetrieveUpdateDestroyView
    Adicionadas as views MovieCreateListView e MovieRetrieveUpdateDestroyView para criar, listar, detalhar, atualizar e deletar filmes no app 'movies'.

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics
+from movies.models import Movie
+from movies.serializers import MovieSerializer
+from django.http import JsonResponse
 
-# Create your views here.
+
+class MovieCreateListView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+    def delete(self, *args, **kwargs):
+        instance = self.get_object()
+        try: 
+            instance.delete()
+            return JsonResponse(
+                {'message': 'Filme deletado com sucesso.'}, 
+                status=204
+            )
+        
+        except Exception as e: 
+            return JsonResponse(
+                {'error': str(e)},
+                status=500
+            )

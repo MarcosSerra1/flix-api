77a4c2b526fedf44c5626a7bd5c8732088808753
Marcos Serra <marcosserra133@gmail.com>
2024-06-26 19:01:30 -0300

    🔍 style: Adicionado modelo Review ao admin
    Registrado o modelo Review no painel de administração do Django para gerenciamento, exibindo os campos movie, stars, e comment na lista de exibição.

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from reviews.models import Review
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('movie', 'stars', 'comment')

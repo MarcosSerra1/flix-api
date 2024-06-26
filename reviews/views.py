6e47e46c8ebc9292767e27fcd6ed9b029ba5bbae
Marcos Serra <marcosserra133@gmail.com>
2024-06-26 18:56:35 -0300

    ✨ feat: Criadas views para o app reviews
    Implementadas as views ReviewCreateListView e ReviewRetrieveUpdateDestroyView para gerenciar operações CRUD na API para o modelo Review.

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics
+from reviews.models import Review
+from reviews.serializers import ReviewSerializer
+from django.http import JsonResponse
 
-# Create your views here.
+
+class ReviewCreateListView(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class ReviewRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def delete(self, *args, **kwargs):
+        instance = self.get_object()
+        try: 
+            instance.delete()
+            return JsonResponse(
+                {'message': 'Filme deletado com sucesso.'}, 
+                status=204
+            )
+        
+        except Exception as e: 
+            return JsonResponse(
+                {'error': str(e)},
+                status=500
+            )

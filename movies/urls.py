0b0c98c896fc81ad57739fa3e9019940228bdee6
Marcos Serra <marcosserra133@gmail.com>
2024-06-26 14:48:01 -0300

    🚏 feat: Criação de rotas no app 'movies'
    Adicionadas as rotas para as views MovieCreateListView e MovieRetrieveUpdateDestroyView no app 'movies', permitindo a criação, listagem, atualização e remoção de filmes.

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
 
 
 urlpatterns = [
-
+    path('movies/', MovieCreateListView.as_view(), name='movies-create-list'),
+    path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movies-detail-view'),
 ]

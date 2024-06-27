6529eab94bc1bca214c0ff9effbafd75184bfa05
Marcos Serra <marcosserra133@gmail.com>
2024-06-27 13:24:17 -0300

    🚀 feat: Modificado urls.py do app principal para seguir as melhores práticas e ajustes com Flake8
    Alterado para incluir os aplicativos genres, ctors, movies, e eviews sob o prefixo pi/v1/ para uma melhor organização e versionamento da API. Realizados ajustes no código para conformidade com as regras do Flake8.

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from django.urls import path
 from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('genres.urls')),
-    path('', include('actors.urls')),
-    path('', include('movies.urls')),
-    path('', include('reviews.urls')),
+
+    path('api/v1/', include('genres.urls')),
+    path('api/v1/', include('actors.urls')),
+    path('api/v1/', include('movies.urls')),
+    path('api/v1/', include('reviews.urls')),
 ]

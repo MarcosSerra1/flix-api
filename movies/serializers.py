3cdff3d79b3e7ef95e935dc87c71f37894887aaa
Marcos Serra <marcosserra133@gmail.com>
2024-07-01 17:18:05 -0300

    ✨ feat: Inserida validação no MovieSerializer para data de lançamento
    Adicionada validação no MovieSerializer para garantir que a data de lançamento não seja anterior a 1990, melhorando a integridade dos dados.

--- movies/serializers.py
+++ movies/serializers.py
@@ -7,3 +7,9 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+    
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990.')
+        
+        return value

afb22ff274ba2d3a6a1bb70a75200231b258a5cb
Marcos Serra <marcosserra133@gmail.com>
2024-07-01 17:25:28 -0300

    ✨ feat: Adicionada validação no MovieSerializer para campo de resumo
    Incluída validação no MovieSerializer para garantir que o resumo não exceda 200 caracteres, assegurando conformidade com os requisitos de tamanho do campo.

--- movies/serializers.py
+++ movies/serializers.py
@@ -11,5 +11,9 @@ class MovieSerializer(serializers.ModelSerializer):
     def validate_release_date(self, value):
         if value.year < 1990:
             raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990.')
-        
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError('Resumo não deve ser maior do que 200 caracteres.')
         return value
